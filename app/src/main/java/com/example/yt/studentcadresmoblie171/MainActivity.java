5abfae1dbc399e20ac4eb9d90a6a7124a1acf166
qiudacheng <qdc0802>
2019-01-04 11:57:27 +0800

    新增

--- app/src/main/java/com/example/yt/studentcadresmoblie171/MainActivity.java
+++ app/src/main/java/com/example/yt/studentcadresmoblie171/MainActivity.java
@@ -1,18 +1,23 @@
 package com.example.yt.studentcadresmoblie171;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 private  EditText edtTxt_main_name;
 private  EditText edtTxt_main_password;
-private Button btn_main_set;
+private Button btn_main_set,btn_main_exit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,18 +28,41 @@ private Button btn_main_set;
         setContentView(R.layout.activity_main);
         edtTxt_main_name=(EditText)findViewById(R.id.edtTxt_main_name);
         edtTxt_main_password=(EditText)findViewById(R.id.edtTxt_main_password);
+        btn_main_exit=((Button)findViewById(R.id.btn_main_exit));
+        btn_main_exit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                this.exit();
+            }
+
+            private void exit() {
+                Dialog dialog=new AlertDialog.Builder(MainActivity.this)
+                        .setTitle("程序退出？")
+                        .setMessage("您确定退出么？")
+                        .setIcon(R.drawable.ic_launcher_background)
+                        .setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                               MainActivity.this.finish();
+                            }
+                        }).setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+
+                            }
+                        }).create();
+                dialog.show();;
+            }
+
+        });
+
         btn_main_set=(Button)findViewById(R.id.btn_main_set);
         btn_main_set.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
             passDate();
             }
-
-
-
-
         });
-
     }
 
     public void passDate() {
--- app/src/main/java/com/example/yt/studentcadresmoblie171/activity_show.java
+++ app/src/main/java/com/example/yt/studentcadresmoblie171/activity_show.java
@@ -1,15 +1,21 @@
 package com.example.yt.studentcadresmoblie171;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class activity_show extends AppCompatActivity {
 
 private TextView tv_show_name;
 private TextView tv_show_password;
+private Button btn_show_jinru,btn_show_exit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,10 +27,36 @@ private TextView tv_show_password;
         Intent intent=getIntent();
         String name=intent.getStringExtra("name");
         String password=intent.getStringExtra("password");
+        btn_show_jinru=(Button)findViewById(R.id.btn_show_jinru);
+        btn_show_exit=(Button)findViewById(R.id.btn_show_exit);
         tv_show_name=(TextView)findViewById(R.id.tv_show_name);
         tv_show_password=(TextView)findViewById(R.id.tv_show_password);
         tv_show_name.setText("用户名："+name);
         tv_show_password.setText("密码："+password);
+btn_show_exit.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View view) {
+        this.exit();
+    }
 
+    private void exit() {
+        Dialog dialog=new AlertDialog.Builder(activity_show.this)
+                .setTitle("程序退出？")
+                .setMessage("您确定退出么？")
+                .setIcon(R.drawable.ic_launcher_background)
+                .setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        activity_show.this.finish();
+                    }
+                }).setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                }).create();
+        dialog.show();;
+    }
+});
     }
 }
